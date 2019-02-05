2e3b834f360073b7cb1998116cd55fdcfeea3788
Nick Inman <nick@theoatmeal.com>
2019-02-05 16:47:02 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,13 +1,17 @@
 class StudentsController < ApplicationController
   before_action :set_student, only: :show
-  
+
   def index
     @students = Student.all
   end
 
   def show
+    #@student = Student.find(params[:id])
   end
 
+  def student_path
+    
+  end
   private
 
     def set_student
